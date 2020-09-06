ce4b8e5965632efa9c078e2e5d4131b6410127d9
yoghurt1131 <yoghurt1131@gmail.com>
2020-09-06 14:19:05 +0900

    initial commit of spring-boot sample applicatoin

---
+++ src/main/java/dev/yoghurt/buildImage/BuildImageApplication.java
@@ -0,0 +1,46 @@
+package dev.yoghurt.buildImage;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+import static org.springframework.web.servlet.function.ServerResponse.ok;
+
+@SpringBootApplication
+public class BuildImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildImageApplication.class, args);
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> routes(MessageService messageService) {
+		return route()
+				.GET("/greet", req -> ok().body(messageService.greet()))
+				.GET("/greet/{name}", req -> ok().body(messageService.greet(req.pathVariable("name"))))
+                .build();
+	}
+}
+
+@Service
+class MessageService {
+	Message greet() {
+		return new Message("Hello, World.");
+	}
+
+	Message greet(String name) {
+		return new Message("Hello, " + name + ".");
+	}
+}
+
+@Data
+@AllArgsConstructor
+class Message {
+	private String body;
+}
---
+++ src/test/java/dev/yoghurt/buildImage/BuildImageApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yoghurt.buildImage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
