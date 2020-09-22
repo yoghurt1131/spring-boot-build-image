ec8ed3199c615e76c5d4296bee1d4c28d12eb088
yoghurt1131 <yoghurt1131@gmail.com>
2020-09-22 15:05:22 +0900

    fix. RouterFunction doesn't work in native image?

--- src/main/java/dev/yoghurt/buildImage/BuildImageApplication.java
+++ src/main/java/dev/yoghurt/buildImage/BuildImageApplication.java
@@ -2,10 +2,14 @@ package dev.yoghurt.buildImage;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
@@ -19,12 +23,24 @@ public class BuildImageApplication {
 		SpringApplication.run(BuildImageApplication.class, args);
 	}
 
-	@Bean
+	// @Bean
 	public RouterFunction<ServerResponse> routes(MessageService messageService) {
 		return route()
 				.GET("/greet", req -> ok().body(messageService.greet()))
 				.GET("/greet/{name}", req -> ok().body(messageService.greet(req.pathVariable("name"))))
                 .build();
+	}
+}
+
+@RequiredArgsConstructor
+@RestController
+class MessageController {
+	private MessageService messageService;
+
+	@ResponseBody
+	@GetMapping("/greet")
+	Message greet() {
+		return new Message("Hello, World.");
 	}
 }
 
